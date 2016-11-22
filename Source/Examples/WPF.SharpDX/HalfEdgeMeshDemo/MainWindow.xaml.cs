910f1be03303309e486747db2a6670b35dc517b7
Iluvatar82 <spitefr@gmail.com>
2016-11-22 21:54:15 +0100

    Demo for HalfEdge created

---
+++ Source/Examples/WPF.SharpDX/HalfEdgeMeshDemo/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HalfEdgeMeshDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        /// <summary>
+        /// The ViewModel
+        /// </summary>
+        MainViewModel mViewModel;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Setup the ViewModel
+            mViewModel = new MainViewModel();
+            this.DataContext = mViewModel;
+        }
+    }
+}
